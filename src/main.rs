c053d361aca5a5440cc637123d1abfc4b577987c
canertuzunar <tuzunarcaner@gmail.com>
2024-02-19 14:49:12 +0300

    write method implemented

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,18 @@ mod wal;
 
 
 fn main() {
-    
+    let mut log_instance = wal::Log{
+        mu: std::sync::Mutex::new(()),
+        path: String::from("log"),
+        closed: false,
+        corrupt: false,
+        log_file: std::fs::File::create("log").expect("Error creating File"),
+        first_index: 0,
+        last_index: 0,
+    };
+
+    match log_instance.write() {
+        Ok(_) => println!("Data writen without error"),
+        Err(e) => eprintln!("Error: {}", e)
+    }
 }
--- src/wal/mod.rs
+++ src/wal/mod.rs
@@ -1,81 +1,81 @@
-use std::{fs::File, sync::Mutex};
+use std::{fs::File, io::{self, Write}, sync::Mutex};
 
 
-#[derive(Debug)]
-pub enum LogFormat {
-    Binary,
-    JSON
+// #[derive(Debug)]
+// pub enum LogFormat {
+//     Binary,
+//     JSON
+// }
+
+
+// #[derive(Debug)]
+// pub struct Options {
+//     pub no_sync: bool,
+
+//     pub segment_size: usize,
+
+//     pub log_format: LogFormat,
+
+//     pub segment_cache_size: usize,
+
+//     pub no_copy: bool,
+
+//     pub dir_perms: u32,
+//     pub file_perms: u32,
+// }
+
+// #[derive(Debug)]
+// enum Error {
+//     Corrupt,
+//     Closed,
+// }
+
+pub struct Log {
+    pub mu: Mutex<()>,
+    pub path: String,
+    // opts: Options,
+    pub closed: bool,
+    pub corrupt: bool,
+    pub log_file: File,
+    pub first_index: u64,
+    pub last_index: u64
 }
 
+// impl Options {
+//     fn default() -> Self {
+//         Self {
+//             no_sync: false,
+//             segment_size: 20,
+//             log_format: LogFormat::Binary,
+//             segment_cache_size: 1,
+//             no_copy: false,
+//             dir_perms: 0o755,
+//             file_perms: 0o644 
+//         }
+//     }
+// }
 
-#[derive(Debug)]
-pub struct Options {
-    pub no_sync: bool,
-
-    pub segment_size: usize,
-
-    pub log_format: LogFormat,
-
-    pub segment_cache_size: usize,
-
-    pub no_copy: bool,
-
-    pub dir_perms: u32,
-    pub file_perms: u32,
-}
-
-#[derive(Debug)]
-enum Error {
-    Corrupt,
-    Closed,
-}
-
-struct Log {
-    mu: Mutex<()>,
-    path: String,
-    opts: Options,
-    closed: bool,
-    corrupt: bool,
-    log_file: Option<File>,
-    first_index: u64,
-    last_index: u64
-}
-
-impl Options {
-    fn default() -> Self {
-        Self {
-            no_sync: false,
-            segment_size: 20,
-            log_format: LogFormat::Binary,
-            segment_cache_size: 1,
-            no_copy: false,
-            dir_perms: 0o755,
-            file_perms: 0o644 
-        }
-    }
-}
-
-impl Log {
-    pub fn write(&self, index: u64, data: Vec<u8>){
+ impl Log {
+    pub fn write(&mut self) -> io::Result<()> {
         let _loc = self.mu.lock().unwrap();        
-
-        self.wb
+        self.log_file.write_all(b"hello")?;
+        Ok(())
     }
 
-    pub fn open(path: &str, opt: Option<Options>){
-        let mut opts = opt;
+    // pub fn open(path: &str, opt: Option<Options>){
+    //     let mut opts = opt;
 
-        match opts {
-            Some(p) => {
-                println!("Options: {:?}", p)
-            }
-            None => {
-                opts = Some(Options::default());
-                println!("{:?}", opts)
-            }
-        }; 
+    //     match opts {
+    //         Some(p) => {
+    //             println!("Options: {:?}", p)
+    //         }
+    //         None => {
+    //             opts = Some(Options::default());
+    //             println!("{:?}", opts)
+    //         }
+    //     }; 
 
-    }
+    // }
 }
 
 
