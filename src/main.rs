aa3fab7c6cd6c636f3b4963fc6050abee0207cdf
Caner <tuzunarcaner@gmail.com>
2024-02-29 15:52:32 +0300

    format

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
-mod wal;
 mod segment;
+mod wal;
 
 fn main() {
     let mut wal = wal::Log::open("./log").unwrap();
 
-    let entries = vec!["hello", "from", "wal", "implementation", "another", "segment", "file", "logs", "lmao", "limeyo"];
+    let entries = vec![
+        "hello",
+        "from",
+        "wal",
+        "implementation",
+        "another",
+        "segment",
+        "file",
+        "logs",
+        "lmao",
+        "limeyo",
+    ];
 
-    /*for n in 1..=80000 {
-         let entry = format!("Entry number is {}", n);
-         wal.write(entry.as_bytes());
-    }*/
+    for n in 1..=110000 {
+        let entry = format!("Entry number is {}", n);
+        let _ = wal.write(entry.as_bytes());
+    }
 
     /*for entry in entries {
          wal.write(entry.as_bytes());
     }*/
 
-    wal.read_all();
+    // wal.read_all();
 }
