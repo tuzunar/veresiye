9fd68ff7d0bc2a7de397f2cef751d5c7a37bdeb7
Caner <tuzunarcaner@gmail.com>
2024-06-25 22:39:14 +0300

    memory db added

--- src/memdb/mod.rs
+++ src/memdb/mod.rs
@@ -1,41 +1,61 @@
 use std::collections::HashMap;
 
 pub struct memdb {
-   data: HashMap<String, String>
+    data: HashMap<String, String>,
+    buffer: HashMap<String, String>,
 }
 
 impl memdb {
-   pub fn new() -> memdb {
-      memdb { data: HashMap::new() }
-   }
+    pub fn new() -> memdb {
+        memdb {
+            data: HashMap::new(),
+            buffer: HashMap::new(),
+        }
+    }
 
-   pub fn insert(&mut self, key: &str, value: &str) {
-      self.data.insert(String::from(key), String::from(value));
-   }
+    pub fn insert(&mut self, key: &str, value: &str) {
+        self.buffer.insert(String::from(key), String::from(value));
+    }
 
-   pub fn get(&self, key: &str) -> Option<&String> {
-      self.data.get(key)
-   }
+    pub fn get(&self, key: &str) -> Option<&String> {
+        self.buffer.get(key)
+    }
 
-   pub fn get_hash_table(&self) -> &HashMap<String, String> {
-      &self.data
-   }
+    pub fn get_hash_table(&self) -> &HashMap<String, String> {
+        &self.data
+    }
 
-   pub fn delete(&mut self, key: &str) -> Option<String> {
-      self.data.remove(key)
-   }
+    pub fn delete(&mut self, key: &str) -> Option<String> {
+        self.buffer.remove(key)
+    }
 
-   pub fn size(&self) -> usize {
-      let key_size = std::mem::size_of::<String>();
-      let value_size = std::mem::size_of::<String>();
-      let hashmap_overhead = std::mem::size_of::<HashMap<String, String>>();
-      let entry_overhead = std::mem::size_of::<(String, String)>();
-      let num_entries = self.data.len();
+    pub fn size(&self) -> usize {
+        let key_size = std::mem::size_of::<String>();
+        let value_size = std::mem::size_of::<String>();
+        let hashmap_overhead = std::mem::size_of::<HashMap<String, String>>();
+        let entry_overhead = std::mem::size_of::<(String, String)>();
+        let num_entries = self.buffer.len();
 
-      key_size * num_entries + value_size * num_entries + hashmap_overhead + entry_overhead * num_entries
-   }
+        key_size * num_entries
+            + value_size * num_entries
+            + hashmap_overhead
+            + entry_overhead * num_entries
+    }
 
-   pub fn clear(&mut self) {
-      self.data.clear()
-   }
+    fn clear(&mut self) {
+        self.buffer.clear()
+    }
+
+    pub fn move_buffer_to_data(&mut self) {
+        println!(
+            "before move buffer to data new buffer size is {}",
+            memdb::size(&self)
+        );
+        self.data = self.buffer.clone();
+        self.buffer = HashMap::new();
+        println!(
+            "move buffer to data new buffer size is {}",
+            memdb::size(&self)
+        )
+    }
 }
