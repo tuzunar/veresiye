c1824a1dec138d60b49045d4f5ec818a990ed764
Caner <tuzunarcaner@gmail.com>
2024-11-19 10:30:13 +0300

    mark compacted sstable as removable feature added

--- src/manifest/manifest_data.rs
+++ src/manifest/manifest_data.rs
@@ -1,16 +1,24 @@
+use std::path::PathBuf;
+
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ManifestData {
     pub last_flushed_sequence: String,
     pub last_flushed_segment: String,
+    pub removable_tables: Vec<PathBuf>,
 }
 
 impl ManifestData {
-    pub fn create(last_flushed_sequence: String, last_flushed_segment: String) -> Self {
+    pub fn create(
+        last_flushed_sequence: String,
+        last_flushed_segment: String,
+        removable_tables: Vec<PathBuf>,
+    ) -> Self {
         let last_flushed_sequence = ManifestData {
             last_flushed_sequence,
             last_flushed_segment,
+            removable_tables,
         };
 
         last_flushed_sequence
@@ -18,7 +26,7 @@ impl ManifestData {
 
     pub fn get_serialized(&self) -> Vec<u8> {
         let serialized = serde_json::to_vec(&self).expect("manifest data serialized error");
-
+        println!("{:?}", serialized);
         serialized
     }
 
@@ -35,6 +43,14 @@ impl ManifestData {
 
     pub fn get_segment_name(&self) -> &str {
         &self.last_flushed_segment
+    }
+
+    pub fn get_removable_tables(&self) -> &Vec<PathBuf> {
+        &self.removable_tables
+    }
+
+    pub fn set_removable_tables(&mut self, tables: Vec<PathBuf>) {
+        self.removable_tables = tables;
     }
 
     pub fn set_sequence_number(&mut self, sequence_number: String, segment_name: String) {
--- src/manifest/mod.rs
+++ src/manifest/mod.rs
@@ -1,6 +1,7 @@
 use std::{
     fs::{File, OpenOptions},
     io::{self, Read, Seek, SeekFrom},
+    path::PathBuf, vec,
 };
 
 use manifest_data::ManifestData;
@@ -38,39 +39,71 @@ impl Manifest {
     // }
 
     pub fn get_manifest(&mut self) -> ManifestData {
-        self.file.seek(SeekFrom::Start(0)).unwrap();
+        if self.file.metadata().unwrap().len() as i32 == i32::from(0) {
+            let manifest_content = ManifestData::create(String::from("0"), String::from("./log/00000000000000000001"), vec![]);
+            manifest_content
+        } else {
+            self.file.seek(SeekFrom::Start(0)).unwrap();
 
-        let mut manifest_buffer = Vec::new();
+            let mut manifest_buffer = Vec::new();
 
-        self.file
-            .read_to_end(&mut manifest_buffer)
-            .expect("cannot read manifest file");
+            self.file
+                .read_to_end(&mut manifest_buffer)
+                .expect("cannot read manifest file");
+            let manifest_content: ManifestData = ManifestData::get_deserialized(manifest_buffer);
 
-        let manifest_content: ManifestData = ManifestData::get_deserialized(manifest_buffer);
-
-        manifest_content
+            manifest_content
+        }
     }
+
+    // pub fn get_removable_tables(&mut self) -> Vec<PathBuf> {
+    //     if(self.file.metadata().unwrap().len() == 0){
+    //         let empty_vec: Vec<PathBuf> = vec![];
+    //         empty_vec
+    //     } else {
+    //         let manifest = self.get_manifest();
+    //         manifest.get_removable_tables().to_vec()
+    //     }
+    // }
 
     pub fn edit_manifest(
         &mut self,
         new_sequence_number: String,
-        segment_name: String,
+        segment_name: String
     ) -> ManifestData {
-        let manifest = ManifestData::create(new_sequence_number, segment_name);
+        let manifest =
+            ManifestData::create(new_sequence_number, segment_name, vec![]);
 
+        manifest
+    }
+
+    pub fn edit_removable_tables(&mut self, tables: Vec<PathBuf>) -> ManifestData {
+        let manifest_content = self.get_manifest();
+        let removable_tables = tables;
+
+        let manifest = ManifestData::create(
+            manifest_content.last_flushed_sequence,
+            manifest_content.last_flushed_segment,
+            removable_tables,
+        );
         manifest
     }
 
     pub fn save_manifest(&mut self, manifest: ManifestData) {
         self.file.set_len(0).unwrap();
         self.file.rewind().unwrap();
-        serde_json::to_writer(
-            &self.file,
-            &ManifestData {
-                last_flushed_sequence: String::from(manifest.get_sequence_number()),
-                last_flushed_segment: String::from(manifest.get_segment_name()),
+
+        let last_flushed_sequence = manifest.get_sequence_number();
+        let last_flushed_segment = manifest.get_segment_name();
+        let current_data = &ManifestData {
+            last_flushed_sequence: String::from(last_flushed_sequence),
+            last_flushed_segment: String::from(last_flushed_segment),
+            removable_tables: if manifest.get_removable_tables().len() == 0 {
+                manifest.get_removable_tables().to_vec()
+            } else {
+                vec![]
             },
-        )
-        .expect("cannot update manifest file");
+        };
+        serde_json::to_writer(&self.file, current_data);
     }
 }
