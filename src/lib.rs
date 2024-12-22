3dfdf2b1e999e5a86308667e154e788948c73e41
Caner <tuzunarcaner@outlook.com>
2024-12-22 21:47:44 +0300

    DEL: commnents

--- src/lib.rs
+++ src/lib.rs
@@ -8,34 +8,3 @@ pub mod veresiye;
 mod wal;
 
 pub use veresiye::Veresiye;
-
-// #[tokio::main]
-// pub async fn launch_server() -> Result<(), Box<dyn std::error::Error>> {
-//     let listener = TokioTcpListener::bind("127.0.0.1:19230").await?;
-
-//     loop {
-//         let (socket, _) = listener.accept().await?;
-//         tokio::spawn(async move {
-//             let mut stream = socket;
-
-//             match protocol::handle_connection(&mut stream).await {
-//                 Ok(_) => (),
-//                 Err(e) => {
-//                     eprintln!("Error handling connection: {}", e);
-//                 }
-//             }
-//         });
-//     }
-// }
-
-// mod protocol {
-
-//     use tokio::net::TcpStream;
-
-//     pub async fn handle_connection(stream: &TcpStream) -> Result<(), Box<dyn std::error::Error>> {
-//         // use tokio::io::{AsyncReadExt, AsyncWriteExt};
-
-//         println!("{:?}", stream);
-//         Ok(())
-//     }
-// }
